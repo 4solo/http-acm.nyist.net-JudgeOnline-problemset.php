b3ed1be3e38f826522cadcae4d8c1c397b1ed844
4solo <34310696@qq.com>
2015-06-13 23:59:00 -0700

    problem 98

---
+++ 98.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	while (n != 0)
+	{
+		int grade;
+		cin >> grade;
+		if (grade >= 90)
+			cout << "A" << endl;
+		else if (grade>=80)
+			cout << "B" << endl;
+		else if (grade >= 70)
+			cout << "C" << endl;
+		else if (grade >= 60)
+			cout << "D" << endl;
+		else
+		{
+			cout << "E" << endl;
+		}
+		n--;
+	}
+}
